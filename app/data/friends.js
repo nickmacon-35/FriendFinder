ebc962fc6eb06c27ded2d4b51a70739531aac363
Nick Macon <nickmacon@gmail.com>
2020-01-01 21:18:19 -0600

    1.1 push

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,14 +5,24 @@
 // But you could have it be an empty array as well.
 // ===============================================================================
 
-var tableArray = [
+var friendArray = [
   {
-    customerName: "Ahmed",
-    customerEmail: "ahmed@example.com",
-    customerID: "afhaque89",
-    phoneNumber: "000-000-0000"
+    name:"Super",
+    photo:"https://www.esportspedia.com/streamers/images/5/57/Super.jpeg",
+    scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
   }
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
-module.exports = tableArray;
+module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -34,14 +34,8 @@ module.exports = function(app) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
+      friends.push(req.body);
       res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
   });
 
   // ---------------------------------------------------------------------------
@@ -50,8 +44,7 @@ module.exports = function(app) {
 
   app.post("/api/clear", function(req, res) {
     // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
+    friends.length = 0;
 
     res.json({ ok: true });
   });
